ad0587e488f983a6321b330ad916da7454e00207
malin <malin.rosen@medieinstitutet.se>
2024-04-15 14:19:50 +0200

    done some testing

---
+++ src/__tests__/functions.test.ts
@@ -0,0 +1,41 @@
+import { addTodo, changeTodo, removeAllTodos } from "../ts/functions";
+import { Todo } from "../ts/models/todo";
+
+describe("testing addTodo", ()=>{
+    test("it should add a todo to list", ()=>{
+        //Assign
+        const todoText = "Gå och handla";
+        const theList : Todo[] = [];
+        const beginningLength = theList.length;
+
+        //Act
+        addTodo(todoText, theList);
+        //Assert
+        expect(theList.length).toBe(beginningLength + 1);
+    })
+})
+
+describe("testing changeTodo", ()=>{
+    test("it should change todo", ()=>{
+        //Assign
+        let done:boolean = true;
+        const todo : Todo = new Todo("handla", done);
+       
+        //Act
+        changeTodo(todo);
+        //Assert
+        expect(todo.done).toBeFalsy;
+
+    })
+})
+
+describe("testing removeAllToos", ()=>{
+    test("it should remove todos", ()=>{
+        //Assign
+       const todoList : Todo [] = [new Todo("Städa", true), new Todo("Plugga", true)];
+        //Act
+        removeAllTodos(todoList);
+        //Assert
+        expect(todoList.length).toBe(0);
+    });
+})
